1506f4125a2af84b45759603dd3840c4abf9d3be
marcel riera <mr.marcel.riera@gmail.com>
2019-05-26 15:31:25 -0400

    Removed MATLAB default print from time calculation

--- main.m
+++ main.m
@@ -1,37 +1,51 @@
+SIZE_1 = 36;
+SIZE = 20000;
 
+A = zeros (SIZE_1, 1);
+B = zeros (SIZE, 1);
+C = zeros (SIZE, 1);
 
-A = zeros (34, 1);
-B = zeros (1400, 1);
-C = zeros (1400, 1);
-
-for i = 1:34
+for i = 1:SIZE_1
     tic
-    fibonacci(i)
+    fibonacci(i);
     A(i) = toc;
 end
 
-for i = 1:1400
+for i = 1:SIZE
     tic
-    fibonacciDynamic(i)
+    fibonacciDynamic(i);
     B(i) = toc;
 end
 
-for i = 1:1400
+for i = 1:SIZE
     tic
-    fibonacciMatrix(i)
+    fibonacciMatrix(i);
     C(i) = toc;
 end
 
 figure
 
-subplot (3,1,1)
-plot (A)
+B = rmoutliers(B);
+C = rmoutliers(C);
+
+B = smoothdata(B, 'sgolay');
+C = smoothdata(C, 'sgolay');
+
+subplot (4,1,1)
+plot (1:size(A), A)
 title ('Recursive Fib')
 
-subplot (3,1,2)
-plot (B)
+subplot (4,1,2)
+plot (1:size(B), B)
 title ('Dynamic Iterative Fib')
 
-subplot (3,1,3)
-plot (C)
+subplot (4,1,3)
+plot (1:size(C), C)
 title ('Matrix Exponentiation Fib')
+
+subplot (4,1,4)
+plot (1:size(B), B)
+hold on
+plot (1:size(C), C)
+hold off
+title ('Dynamic Iterative Fib vs Matrix Exponentiation Fib')
